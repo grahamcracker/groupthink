a678a4cf97476dbb8f2ce965b816ae498f1aae58
Graham Ramsey <g@graham.li>
2016-12-10 00:22:52 -0800

    Add default URL for production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
     password:             Settings.gmail.app_password,
     authentication:       'plain',
     enable_starttls_auto: true  }
+  config.action_mailer.default_url_options = { host: Settings.gmail.domain }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
